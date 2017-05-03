1296a18e6d87920fa2079ce4f98a01aa1d3d9146
Sean Lip <sean@seanlip.org>
2017-05-03 00:22:30 -0700

    Update test_util_jobs.py to convert the jobs into one-off ones.

--- core/tests/test_util_jobs.py
+++ core/tests/test_util_jobs.py
@@ -15,8 +15,10 @@
 # limitations under the License.
 
 """Jobs operating on explorations that can be used for production tests.
-To use these jobs, first need to register them in jobs_registry (at
-the moment they are not displayed there to avoid accidental use)."""
+
+To use these jobs, the developer should register them in jobs_registry. (At
+the moment, they are not displayed there, in order to avoid accidental use).
+"""
 
 from core import jobs
 from core.domain import exp_domain
@@ -29,101 +31,41 @@ import feconf
     models.NAMES.base_model, models.NAMES.exploration])
 
 
-class ExpCopiesRealtimeModel(
-        jobs.BaseRealtimeDatastoreClassForContinuousComputations):
-    pass
-
-
-class ExpCopiesAggregator(jobs.BaseContinuousComputationManager):
-    """A continuous-computation job creating 10 published copies of every
-    existing exploration, with the eid being '[old_eid]copy[copy_number]',
+class ExpCopiesOneOffJob(jobs.BaseMapReduceJobManager):
+    """A one-off job that creates 10 published copies of every existing
+    published exploration, with the eid being '[old_eid]copy[copy_number]',
     title 'Copy' and category 'Copies'.
     """
-    @classmethod
-    def get_event_types_listened_to(cls):
-        return []
-
-    @classmethod
-    def _get_realtime_datastore_class(cls):
-        return ExpCopiesRealtimeModel
-
-    @classmethod
-    def _get_batch_job_manager_class(cls):
-        return ExpCopiesMRJobManager
-
-    @classmethod
-    def _handle_incoming_event(cls, active_realtime_layer, event_type, *args):
-        pass
-
-
-class ExpCopiesMRJobManager(
-        jobs.BaseMapReduceJobManagerForContinuousComputations):
-    """A continuous-computation job creating 10 published copies of every
-    existing exploration, with the eid being '[old_eid]copy[copy_number]',
-    title 'Copy' and category 'Copies'.
-    """
-
-    @classmethod
-    def _get_continuous_computation_class(cls):
-        return ExpCopiesAggregator
-
     @classmethod
     def entity_classes_to_map_over(cls):
         return [exp_models.ExplorationModel]
 
     @staticmethod
     def map(item):
-        if ExpCopiesMRJobManager._entity_created_before_job_queued(item):
+        if ExpCopiesOneOffJob._entity_created_before_job_queued(item):
+            exp_rights = rights_manager.get_exploration_rights(item.id)
+            if exp_rights.status == rights_manager.ACTIVITY_STATUS_PRIVATE:
+                return
+
             for count in range(10):
-                yield ('%scopy%d' % (item.id, count),
-                       exp_services.get_exploration_from_model(item).to_yaml())
+                exploration = exp_services.get_exploration_from_model(item)
+                exploration.title = 'Copy'
+                exploration.category = 'Copies'
+                yield ('%scopy%d' % (item.id, count), exploration.to_yaml())
 
     @staticmethod
     def reduce(exp_id, list_of_exps):
         for stringified_exp in list_of_exps:
-            exploration = exp_domain.Exploration.from_untitled_yaml(
-                exp_id, 'Copy', 'Copies', stringified_exp)
+            exploration = exp_domain.Exploration.from_yaml(
+                exp_id, stringified_exp)
             exp_services.save_new_exploration(
                 feconf.SYSTEM_COMMITTER_ID, exploration)
             rights_manager.publish_exploration(
                 feconf.SYSTEM_COMMITTER_ID, exp_id)
 
 
-# Job to delete all copied explorations.
-class DeleteExpCopiesRealtimeModel(
-        jobs.BaseRealtimeDatastoreClassForContinuousComputations):
-    pass
-
-
-class DeleteExpCopiesAggregator(jobs.BaseContinuousComputationManager):
-    """A continuous-computation job deleting all explorations in category
-    'Copies'.
-    """
-    @classmethod
-    def get_event_types_listened_to(cls):
-        return []
-
-    @classmethod
-    def _get_realtime_datastore_class(cls):
-        return DeleteExpCopiesRealtimeModel
-
-    @classmethod
-    def _get_batch_job_manager_class(cls):
-        return DeleteExpCopiesMRJobManager
-
-    @classmethod
-    def _handle_incoming_event(cls, active_realtime_layer, event_type, *args):
-        pass
-
-
-class DeleteExpCopiesMRJobManager(
-        jobs.BaseMapReduceJobManagerForContinuousComputations):
-    """Job that deletes all explorations in category 'Copies'.
-    """
-    @classmethod
-    def _get_continuous_computation_class(cls):
-        return DeleteExpCopiesAggregator
-
+class DeleteExpCopiesOneOffJob(jobs.BaseMapReduceJobManager):
+    """A one-off job that deletes all explorations in the category 'Copies'."""
     @classmethod
     def entity_classes_to_map_over(cls):
         return [exp_models.ExplorationModel]
